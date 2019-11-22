847c4cd6bd1f81bac635ac06297984347c56035d
EmmS21 <emmanuel@hooros.co.za>
2019-11-22 18:28:51 +0200

    changes to app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ import os
 path = 'https://raw.githubusercontent.com/EmmS21/SpringboardCapstoneBoxingPredictionWebApp/master/boxingdata/visuals.csv'
 path_two = 'https://raw.githubusercontent.com/EmmS21/SpringboardCapstoneBoxingPredictionWebApp/master/boxingdata/longformateddata2.csv'
 data = pd.read_csv(path)
-data['division'] = data['division'].fillna('unknown')
-data['bouts_fought'] = data['w'].astype('float')+data['l'].astype('float')+data['d'].astype('float')
 WEIGHT_CLASS = data['division'].unique()
 GENDER = data['sex'].unique()
+data['division'] = data['division'].fillna('unknown')
+data['bouts_fought'] = data['w'].astype('float')+data['l'].astype('float')+data['d'].astype('float')
+
 #uploading data for heatmap
 long_data = pd.read_csv(path_two)
 #count wins by stance
